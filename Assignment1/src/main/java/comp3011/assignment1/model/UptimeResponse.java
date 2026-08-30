5a2c945d6f02d885be8780c38f8ab4dec7af803e
Huydq002 <quanghuy9705@gmail.com>
2026-08-30 20:02:10 +0930

    Add constructor and getter in UptimeResponse model

--- Assignment1/src/main/java/comp3011/assignment1/model/UptimeResponse.java
+++ Assignment1/src/main/java/comp3011/assignment1/model/UptimeResponse.java
@@ -1,5 +1,16 @@
 package comp3011.assignment1.model;
 
 public class UptimeResponse {
-
+	private String utcServerStart; // UTC time server started
+	private String utcNow; 	// Current time
+	private double serverUptimeSeconds; // server started and current time gap
+	
+	public UptimeResponse(String utcServerStart,String utcNow, double serverUptimeSeconds) {
+		this.utcServerStart = utcServerStart;
+		this.utcNow = utcNow;
+		this.serverUptimeSeconds = serverUptimeSeconds;
+	}
+	public String getutcServerStart() {return utcServerStart;}
+	public String getutcNow() {return utcNow;}
+	public double getserverUptimeSeconds() {return serverUptimeSeconds;}
 }
