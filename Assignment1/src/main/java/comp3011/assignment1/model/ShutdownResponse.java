010fd102a6ba35c1fef475169a5f6a2963f0177e
Huydq002 <quanghuy9705@gmail.com>
2026-08-30 20:05:16 +0930

    Add constructor and getter in ShutDownResponse model

--- Assignment1/src/main/java/comp3011/assignment1/model/ShutdownResponse.java
+++ Assignment1/src/main/java/comp3011/assignment1/model/ShutdownResponse.java
@@ -1,5 +1,11 @@
 package comp3011.assignment1.model;
 
 public class ShutdownResponse {
-
+	private String message; // UTC time server started
+	
+	public ShutdownResponse(String message) {
+		this.message = message;
+		
+	}
+	public String getMessage() {return message;}
 }
