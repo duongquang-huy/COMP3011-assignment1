0b1f49c51d3586d0d57426657ed5ac226689758a
Huydq002 <quanghuy9705@gmail.com>
2026-08-30 20:19:19 +0930

    Add constructor and getter in ErorrResponse model

--- Assignment1/src/main/java/comp3011/assignment1/model/ErrorResponse.java
+++ Assignment1/src/main/java/comp3011/assignment1/model/ErrorResponse.java
@@ -1,5 +1,22 @@
 package comp3011.assignment1.model;
 
 public class ErrorResponse {
-
+	private String timestamp; // UTC timestamp at which the error 
+	private int status; // HTTP status code
+	private String error; 	// Short HTTP reason phrase or error
+	private String message; // Human-readable description
+	private String path;  // Request path that produced the error
+	
+	public ErrorResponse(String timestamp,String error, String message, int status, String path) {
+		this.timestamp = timestamp;
+		this.status = status;
+		this.error = error;
+		this.message = message;
+		this.path = path;
+	}
+	public String getTimestamp() {return timestamp;}
+	public int getStatus() {return status;}
+	public String getError() {return error;}
+	public String getMessage() {return message;}
+	public String getPath() {return path;}
 }
