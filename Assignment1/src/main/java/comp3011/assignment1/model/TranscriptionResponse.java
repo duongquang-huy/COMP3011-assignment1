a91d62c13c2bbd2e135f567e38ca93acfaf5b1e5
Huydq002 <quanghuy9705@gmail.com>
2026-09-04 20:58:45 +0930

    Add Transcripton Response model with getter

---
+++ Assignment1/src/main/java/comp3011/assignment1/model/TranscriptionResponse.java
@@ -0,0 +1,10 @@
+package comp3011.assignment1.model;
+
+public class TranscriptionResponse {
+	private String text;
+	
+	public TranscriptionResponse(String text) {
+		this.text = text;
+	}
+	public String getText() {return text;}
+}
